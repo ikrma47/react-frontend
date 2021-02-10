60fa77cff4f3bc7560fc743528931f109d3d18bf
ikrma47 <ikrmaahmad47@gmail.com>
2021-02-10 23:44:09 +0500

    made the admin dashboard page dynamic

--- src/pages/Admin/Dashboard/index.jsx
+++ src/pages/Admin/Dashboard/index.jsx
@@ -1,8 +1,62 @@
-import React from "react"
-import DisplayDashboard from "components/DisplayComponent/DisplayDashboard"
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Spinner as CenteredSpinner } from "elements/Spinner";
+import DisplayDashboard from "components/DisplayComponent/DisplayDashboard";
+import { getAdminDashboardAction } from "pages/Admin/Dashboard/ducks/actions";
+import { BorderedButton, FilledButton } from "elements/Button";
 
-const Dashboard = () => {
-    return <DisplayDashboard />
-}
+const tableHeads = [
+  "user",
+  "app#ID",
+  "Name",
+  "Course Category",
+  "Status",
+  "Accept",
+  "Reject",
+];
+const message = "Waiting to be accepted";
 
-export default Dashboard
+const Dashboard = ({ dashboard, getAdminDashboardAction }) => {
+  useEffect(() => {
+    async function getAdminDashboard() {
+      await getAdminDashboardAction();
+    }
+
+    getAdminDashboard();
+  }, []);
+
+  const acceptHandler = (appId) => console.log(appId);
+  const rejectHandler = (appId) => console.log(appId);
+
+  const AcceptButton = (props) => {
+    return (
+      <BorderedButton {...props} type="button">
+        Accept
+      </BorderedButton>
+    );
+  };
+
+  const RejectButton = (props) => {
+    return (
+      <FilledButton {...props} type="button">
+        Reject
+      </FilledButton>
+    );
+  };
+
+  if (dashboard?.success)
+    return (
+      <DisplayDashboard
+        tableHeads={tableHeads}
+        applicants={dashboard.data}
+        message={message}
+        actionButtons={[AcceptButton, RejectButton]}
+        buttonHandlers={[acceptHandler, rejectHandler]}
+      />
+    );
+  else return <CenteredSpinner />;
+};
+
+const mapStateToProps = (state) => ({ dashboard: state?.admin?.dashboard });
+
+export default connect(mapStateToProps, { getAdminDashboardAction })(Dashboard);
