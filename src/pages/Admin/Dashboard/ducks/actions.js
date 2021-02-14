cde68c83e5b1c685b9723bfba0b746726bcc6cea
ikrma47 <ikrmaahmad47@gmail.com>
2021-02-14 16:59:55 +0500

    modified admin dashboard ducks

--- src/pages/Admin/Dashboard/ducks/actions.js
+++ src/pages/Admin/Dashboard/ducks/actions.js
@@ -1,11 +1,24 @@
-import { GET_ADMIN_DASHBOARD } from "pages/Admin/Dashboard/ducks/action-types";
-import { getAdminDashboard } from 'api';
+import {
+	GET_ADMIN_DASHBOARD,
+	UPDATE_APPLICATION_STATUS_BY_ADMIN,
+} from "pages/Admin/Dashboard/ducks/action-types";
+import { getAdminDashboard, updateApplicationStatusByAdmin } from "api";
 
-export const getAdminDashboardAction = () => async dispatch => {
-  try {
-    let { data } = await getAdminDashboard()
-    dispatch({ type: GET_ADMIN_DASHBOARD, payload: data })
-  } catch (error) {
-    console.log(error)
-  }
-}
+export const getAdminDashboardAction = () => async (dispatch) => {
+	try {
+		let { data } = await getAdminDashboard();
+		dispatch({ type: GET_ADMIN_DASHBOARD, payload: data });
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const updateApplicationStatusByAdminAction = (appId, dataObj) => async (dispatch) => {
+	try {
+		await updateApplicationStatusByAdmin(appId, dataObj);
+		let { data } = await getAdminDashboard();
+		dispatch({ type: UPDATE_APPLICATION_STATUS_BY_ADMIN, payload: data });
+	} catch (error) {
+		console.log(error);
+	}
+};
