b6985f5ad66879c185a01dec361cfdcb750ec1c9
ikrma47 <ikrmaahmad47@gmail.com>
2021-02-14 17:02:17 +0500

    added select and un select actions

--- src/pages/App/ducks/reducer.js
+++ src/pages/App/ducks/reducer.js
@@ -1,21 +1,29 @@
 import {
-  GET_APPLICATION_STATUS,
-  UPDATE_APPLICATION_STATUS,
+	SELECTED_APPID,
+	UNSELECT_APPID,
+	GET_APPLICATION_STATUS,
+	UPDATE_APPLICATION_STATUS,
 } from "pages/App/ducks/action-types";
 
 const initialState = {};
 
 function reducer(state = initialState, action) {
-  switch (action.type) {
-    case GET_APPLICATION_STATUS:
-      state = { ...state, ...action.payload };
-      break;
-    case UPDATE_APPLICATION_STATUS:
-      state = { ...state, ...action.payload };
-      break;
-    default:
-  }
-  return state;
+	switch (action.type) {
+		case GET_APPLICATION_STATUS:
+			state = { ...state, ...action.payload };
+			break;
+		case UPDATE_APPLICATION_STATUS:
+			state = { ...state, ...action.payload };
+			break;
+		case SELECTED_APPID:
+			state = { ...state, selectedAppId: action.payload };
+			break;
+		case UNSELECT_APPID:
+			state = { ...state, selectedAppId: action.payload };
+			break;
+		default:
+	}
+	return state;
 }
 
 export default reducer;
