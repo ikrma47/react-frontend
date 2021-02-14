98de0b2c48e4127cad1ced443e89d350af23a689
ikrma47 <ikrmaahmad47@gmail.com>
2021-02-14 17:01:44 +0500

    change the state into admin and user level

--- src/pages/App/adminReducer.js
+++ src/pages/App/adminReducer.js
@@ -1,4 +1,18 @@
-import { combineReducers } from 'redux';
-import dashboard from "pages/Admin/Dashboard/ducks/reducer"
+import { combineReducers } from "redux";
+import dashboard from "pages/Admin/Dashboard/ducks/reducer";
+import profile from "pages/Admin/ApplicationForm/Profile/ducks/reducer";
+import academics from "pages/Admin/ApplicationForm/Academics/ducks/reducer";
+import experience from "pages/Admin/ApplicationForm/Experience/ducks/reducer";
+import preference from "pages/Admin/ApplicationForm/Preference/ducks/reducer";
+import documents from "pages/Admin/ApplicationForm/Documents/ducks/reducer";
+import submit from "pages/Admin/ApplicationForm/Submit/ducks/reducer";
 
-export default combineReducers({ dashboard });
+export default combineReducers({
+	dashboard,
+	profile,
+	academics,
+	experience,
+	preference,
+	documents,
+	submit,
+});
