88b24626da238d12d2c9d1eb869b39c8a82b1f00
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-09 11:17:24 +0500

    rectify dashboard data keys

--- src/components/DisplayComponent/DisplayDashboard.jsx
+++ src/components/DisplayComponent/DisplayDashboard.jsx
@@ -5,69 +5,77 @@ import unavailable from "assets/img/unavailable.png";
 const defaultHandler = () => {};
 
 const DisplayDashboard = ({
-	message = "",
-	tableHeads = [],
-	applicants = [],
-	handleClick = defaultHandler,
+  message = "",
+  tableHeads = [],
+  applicants = [],
+  handleClick = defaultHandler,
 }) => {
-	return (
-		<div className="col-lg-12 grid-margin stretch-card mt-4">
-			<Card>
-				<Card.Body>
-					<Card.Title>Welcome to Dashboard</Card.Title>
-					<Card.Text>user description</Card.Text>
-					<Table hover responsive>
-						<thead>
-							<tr>
-								{tableHeads?.map(function displayTableHeading(heading, idx) {
-									return <th key={idx}>{heading}</th>;
-								})}
-							</tr>
-						</thead>
-						<tbody>
-							{applicants?.map(function displayApplicantData(data) {
-								return (
-									<tr key={`${data?.name} ${data?.appId}`}>
-										<td
-											key={`${data?.appId} ${data?.image}`}
-											className="py-2"
-											onClick={() => handleClick(data?.appId)}
-										>
-											<Image
-												key={data?.image || unavailable}
-												src={data?.image || unavailable}
-												width="35px"
-												height="35px"
-												roundedCircle
-											/>
-										</td>
-										<td key={data?.appId || "appId"} onClick={() => handleClick(data?.appId)}>
-											{data?.appId || "-"}
-										</td>
-										<td key={data?.name || "name"} onClick={() => handleClick(data?.appId)}>
-											{data?.name || "-"}
-										</td>
-										<td
-											key={`${data?.appId} ${data?.courseCategory || "courseCategory"}`}
-											onClick={() => handleClick(data?.appId)}
-										>
-											{data?.courseCategory || "-"}
-										</td>
-										<td
-											key={`${data?.name} ${message || "message"}`}
-											onClick={() => handleClick(data?.appId)}
-										>
-											{message}
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</Table>
-				</Card.Body>
-			</Card>
-		</div>
-	);
+  return (
+    <div className="col-lg-12 grid-margin stretch-card mt-4">
+      <Card>
+        <Card.Body>
+          <Card.Title>Welcome to Dashboard</Card.Title>
+          <Card.Text>user description</Card.Text>
+          <Table hover responsive>
+            <thead>
+              <tr>
+                {tableHeads?.map(function displayTableHeading(heading, idx) {
+                  return <th key={idx}>{heading}</th>;
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {applicants?.map(function displayApplicantData(data) {
+                return (
+                  <tr key={`${data?.name} ${data?.user?.appId}`}>
+                    <td
+                      key={`${data?.user?.appId} ${data?.image}`}
+                      className="py-2"
+                      onClick={() => handleClick(data?.user?.appId)}
+                    >
+                      <Image
+                        key={data?.image || unavailable}
+                        src={data?.image || unavailable}
+                        width="35px"
+                        height="35px"
+                        roundedCircle
+                      />
+                    </td>
+                    <td
+                      key={data?.user?.appId || "appId"}
+                      onClick={() => handleClick(data?.user?.appId)}
+                    >
+                      {data?.user?.appId || "-"}
+                    </td>
+                    <td
+                      key={data?.name || "name"}
+                      onClick={() => handleClick(data?.user?.appId)}
+                    >
+                      {data?.name || "-"}
+                    </td>
+                    <td
+                      key={`${data?.user?.appId} ${
+                        data?.courseCategory || "courseCategory"
+                      }`}
+                      onClick={() => handleClick(data?.user?.appId)}
+                    >
+                      {data?.courseCategory || "-"}
+                    </td>
+                    <td
+                      key={`${data?.name} ${message || "message"}`}
+                      onClick={() => handleClick(data?.user?.appId)}
+                    >
+                      {message}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default DisplayDashboard;
