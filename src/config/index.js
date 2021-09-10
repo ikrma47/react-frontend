0f4fec47cb99ba535eed4be4556a589e8c4ccd00
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-10 21:07:50 +0500

    generalize the api base path

--- src/config/index.js
+++ src/config/index.js
@@ -5,8 +5,7 @@ const {
 } = process.env;
 
 export const IS_PRODUCTION = REACT_APP_IS_PRODUCTION === "false";
-export const API_BASE_PATH_USER = REACT_APP_API_URL.toString();
-export const API_BASE_PATH_ADMIN = REACT_APP_API_URL.toString() + "/admin";
+export const API_BASE_PATH = REACT_APP_API_URL.toString();
 export const SENTRY_URL = REACT_APP_SENTRY_URL.toString();
 export const APP_NAME = "PGS Admission System";
 export const COOKIE_USER_TOKEN_FIELD = "authToken";
