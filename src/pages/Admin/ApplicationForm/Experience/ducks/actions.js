90d9a79207d4ec8d521cbcaeb53b8e19e6094439
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-11 09:08:53 +0500

    dispatch the error request

--- src/pages/Admin/ApplicationForm/Experience/ducks/actions.js
+++ src/pages/Admin/ApplicationForm/Experience/ducks/actions.js
@@ -1,11 +1,16 @@
 import { GET_EXPERIENCE_OF_SELECTED_APPID } from "pages/Admin/ApplicationForm/Experience/ducks/action-types";
 import { getExperienceById } from "api";
 
-export const getExperienceOfSelectedAppIdAction = (appId) => async (dispatch) => {
-	try {
-		let { data } = await getExperienceById(appId);
-		dispatch({ type: GET_EXPERIENCE_OF_SELECTED_APPID, payload: data });
-	} catch (error) {
-		console.log(error);
-	}
+export const getExperienceOfSelectedAppIdAction = (appId) => async (
+  dispatch
+) => {
+  try {
+    let { data } = await getExperienceById(appId);
+    dispatch({ type: GET_EXPERIENCE_OF_SELECTED_APPID, payload: data });
+  } catch (error) {
+    dispatch({
+      type: GET_EXPERIENCE_OF_SELECTED_APPID,
+      payload: error.response.data,
+    });
+  }
 };
