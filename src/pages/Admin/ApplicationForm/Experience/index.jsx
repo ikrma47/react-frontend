d56efd4791908dcc62b58651bae859eb78a7657a
ikrma47 <ikrmaahmad47@gmail.com>
2021-09-11 09:08:28 +0500

    rectified and formatted with prettier

--- src/pages/Admin/ApplicationForm/Experience/index.jsx
+++ src/pages/Admin/ApplicationForm/Experience/index.jsx
@@ -4,25 +4,44 @@ import DisplayExperience from "components/DisplayComponent/DisplayExperience";
 import { Spinner as CenteredSpinner } from "elements/Spinner";
 import { getExperienceOfSelectedAppIdAction } from "pages/Admin/ApplicationForm/Experience/ducks/actions";
 
-const tableHeads = ["Job Title", "Organization", "To", "From", "Salary/ Grade", "Designation"];
+const tableHeads = [
+  "Job Title",
+  "Organization",
+  "To",
+  "From",
+  "Salary/ Grade",
+  "Designation",
+];
 
-const Experience = ({ appId, history, experience, getExperienceOfSelectedAppIdAction }) => {
-	useEffect(() => {
-		async function getExperienceById(appId) {
-			if (appId) await getExperienceOfSelectedAppIdAction(appId);
-			else history.push("/admin/dashboard");
-		}
+const Experience = ({
+  appId,
+  history,
+  experience,
+  getExperienceOfSelectedAppIdAction,
+}) => {
+  useEffect(() => {
+    async function getExperienceById(appId) {
+      if (appId) await getExperienceOfSelectedAppIdAction(appId);
+      else history.push("/admin/dashboard");
+    }
 
-		getExperienceById(appId);
-	}, []);
-	if (experience?.success)
-		return <DisplayExperience tableHeads={tableHeads} experiences={experience.data} />;
-	else return <CenteredSpinner />;
+    getExperienceById(appId);
+  }, []);
+  if (experience?.success || experience?.data)
+    return (
+      <DisplayExperience
+        tableHeads={tableHeads}
+        experiences={experience.data}
+      />
+    );
+  else return <CenteredSpinner />;
 };
 
 const mapStateToProps = (state) => ({
-	appId: state.app.selectedAppId,
-	experience: state.admin.experience,
+  appId: state.app.selectedAppId,
+  experience: state.admin.experience,
 });
 
-export default connect(mapStateToProps, { getExperienceOfSelectedAppIdAction })(Experience);
+export default connect(mapStateToProps, { getExperienceOfSelectedAppIdAction })(
+  Experience
+);
